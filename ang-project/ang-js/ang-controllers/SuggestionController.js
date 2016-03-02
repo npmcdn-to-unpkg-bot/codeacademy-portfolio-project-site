dd5df25a8c3214f1af561cb77b7ff0a66ca05762
ptums <ptumulty923@gmail.com>
2016-03-01 22:45:56 -0500

    updated version

--- ang-project/ang-js/ang-controllers/SuggestionController.js
+++ ang-project/ang-js/ang-controllers/SuggestionController.js
@@ -2,5 +2,21 @@ app.controller('SuggestionController', ['$scope', '$routeParams', 'suggestions',
 
 	$scope.post = suggestions.posts[$routeParams.id];
 
+	console.log($scope.post);
+
+	$scope.addComment = function(){
+		if(!$scope.new_comment || $scope.new_comment === ""){
+			return;
+		}
+		$scope.post.push({
+			body: $scope.new_comment,
+			upvotes: 0
+		});
+		$scope.new_comment = "";
+	}
+
+	
+
+	$scope.new_comment = "";
 
 }]);
