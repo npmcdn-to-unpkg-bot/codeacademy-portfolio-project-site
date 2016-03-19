150a0a89860dc19a52b5fd6473a481e536098ee9
ptums <ptumulty923@gmail.com>
2016-03-18 22:31:44 -0400

    compelted angular project

--- ang-project/ang-js/ang-controllers/SuggestionController.js
+++ ang-project/ang-js/ang-controllers/SuggestionController.js
@@ -1,26 +1,31 @@
 app.controller('SuggestionController', [
 	'$scope',
-	'$routeParams', 
+	'$routeParams',
 	'suggestions',
 
-	function($scope, $routeParams, suggestions) {
-	$scope.post = suggestions.posts[$routeParams.id];
+	function($scope, $routeParams, suggestions){
+		
+		// put individual post ID in an object
+		$scope.post = suggestions.posts[$routeParams.id];
 
-	$scope.addComment =function() {
-		//if input empty don't submit
-		if(!$scope.body || $scope.body === ""){
-			return;
-		}
-		//push comment to a suggestion post
-		$scope.post.comments.push({
-			body: $scope.body,
-			upvotes: 0,
-		});
-		//after submit, clear input
-		$scope.body = " ";
-	}
-	$scope.upVote = function(comment){
-		comment.upvotes+=1;
-	};
+		$scope.addComment = function(){
+		 	
+		 	// if input empty, don't submit
+		 	if(!$scope.body || $scope.body === '') { 
+		  		return; 
+		  	}
+		  	
+		  	// push suggestion posts in suggestions.js
+			$scope.post.comments.push({
+			    body: $scope.body,
+			    upvotes: 0
+			  });
 
-}]);
+			// after submit, clear input
+			$scope.body = '';
+		};
+
+		$scope.upVote = function(comment) {
+			comment.upvotes += 1;
+		};
+	}]);
--- ang-project/ang-js/app.js
+++ ang-project/ang-js/app.js
@@ -1,16 +1,21 @@
-var app = angular.module('SuggestionBox',['ngRoute']);
+var app = angular.module('SuggestionBox', ['ngRoute']);
 
 app.config(function ($routeProvider) {
   $routeProvider
+    // route for the home page
     .when('/', {
-      controller: "HomeController",
-      templateUrl: "ang-project/views/home.html"
+      controller: 'HomeController',
+      templateUrl: 'ang-project/views/home.html'
     })
+
+    //route for individual suggestion page
     .when('/suggestion/:id', {
-	  controller: "SuggestionController",
-	  templateUrl: "ang-project/views/suggestions.html"
-	})
-    .otherwise({
-      redirectTo: '/'
-    });
+      controller: 'SuggestionController',
+      templateUrl: 'ang-project/views/suggestions.html'
+    })
+
+    // otherwise, go home
+    .otherwise({ 
+        redirectTo: '/' 
+      }); 
 });
